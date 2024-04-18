a03008ed618cbbf589ed01d3a080d5b43a3010d9
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-04-18 14:25:14 +0900

    計算ロジックをクラスとして分離

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     internal class Program {
         static void Main(string[] args) {
-   
+
             if (args.Length >= 1 && args[0] == "-tom") {
+
                 //フィートからメートルへの対応表を出力
                 PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
             } else {
@@ -19,30 +20,19 @@ namespace DistanceConverter {
         }
 
         private static void PrintMeterToFeetList(int start, int stop) {
+            FeetConverter fc = new FeetConverter();
             for (int meter = start; meter <= stop; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet =fc.MeterToFeet(meter);
                 Console.WriteLine("{0}m ={1:0.0000}ft", meter, feet);
             }
         }
 
         private static void PrintFeetToMeterList(int start, int stop) {
+            FeetConverter fc = new FeetConverter();
             for (int feet = start; feet <= stop; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = fc.FeetToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
             }
         }
-
-        //フィートからメートルを求める
-        static double FeetToMeter(int feet) {
-                return feet * 0.3048;
-            }
-
-            //メートルからフィートを求める
-            static double MeterToFeet(int meter) {
-                return meter / 0.3048;
-            }
-
-
-        }
     }
-
+}
