e3f2d8f990caca8d68a6aa96f726fc591cbb2356
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-05-08 13:59:32 +0900

    左クリックでサッカーボール出現＆テニスボールクラスの作成

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -2,6 +2,7 @@ namespace BallApp {
     public partial class Form1 : Form {
         SoccerBall soccerBall;
         PictureBox pb;
+        TennisBall tennisBall;
         //コンストラクタ
         public Form1() {
             InitializeComponent();
@@ -13,7 +14,10 @@ namespace BallApp {
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
+
+
             soccerBall.Move();
+
             pb.Location = new Point((int)(soccerBall.PosX), (int)(soccerBall.PosY));
 
         }
@@ -22,14 +26,14 @@ namespace BallApp {
 
             pb = new PictureBox();//画像を表示するコントロール
             pb.Size = new Size(50, 50);
-           /* Point mp = Control.MousePosition;
-            Point pt= this.PointToClient(mp);
-            int x = pt.X;
-            int y = pt.Y;*/
-            soccerBall = new SoccerBall(e.X,e.Y);
 
-            pb.Image = soccerBall.Image;
-            pb.Location = new Point((int)(soccerBall.PosX), (int)(soccerBall.PosY));
+            if (e.Button == MouseButtons.Left) {
+                soccerBall = new SoccerBall(e.X, e.Y);
+                pb.Image = soccerBall.Image;
+                pb.Location = new Point((int)(soccerBall.PosX), (int)(soccerBall.PosY));
+            }
+            
+
             pb.SizeMode = PictureBoxSizeMode.StretchImage;
             pb.Parent = this;
 
