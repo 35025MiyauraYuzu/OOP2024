85d2ba3bf8bcc7c19341c604f66ea79e7fc0d53b
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-05-22 10:30:58 +0900

    演習問題　3.2.1 解答

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercise02 {
@@ -11,21 +13,39 @@ namespace Exercise02 {
                  "Tokyo", "New Delhi", "Bangkok", "London",
                 "Paris", "Berlin", "Canberra", "Hong Kong",
             };
-
+            Console.WriteLine("***3.2.1***");
             Exercise2_1(names);
             Console.WriteLine();
+
+            Console.WriteLine("***3.2.2***");
             Exercise2_2(names);
             Console.WriteLine();
+
+            Console.WriteLine("***3.2.3***");
             Exercise2_3(names);
             Console.WriteLine();
+
+            Console.WriteLine("***3.2.4***");
             Exercise2_4(names);
         }
 
         private static void Exercise2_1(List<string> names) {
-        
+            Console.WriteLine("都市名を入力。空行で終了");
+
+            do {
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(line))
+                    break;//空行だと抜ける
+
+                Console.WriteLine(names.FindIndex(s => s == line));
+
+            } while (true);
+
         }
 
         private static void Exercise2_2(List<string> names) {
+           
         }
 
         private static void Exercise2_3(List<string> names) {
