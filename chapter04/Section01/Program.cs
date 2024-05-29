6139a0bd16e4118a9b02855dfca01e45c5b36715
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-05-29 10:02:26 +0900

    4章完了

--- chapter04/Section01/Program.cs
+++ chapter04/Section01/Program.cs
@@ -9,32 +9,43 @@ namespace Section01 {
 
         static void Main(string[] args) {
             #region null合体演算子
-            string code = "12345";
+            /*string code = "12345";
             var mesage = Getmesage(code) ?? DefaultMesaga();
-            Console.WriteLine(mesage);
+            Console.WriteLine(mesage);*/
 
             #endregion
-            
-            #region  null条件演算子
-            /* Sale sale = new Sale {
-                 Amount = 1000,
 
-             };*/
-            Sale sale = null;
+            #region  null条件演算子
+            /* /* Sale sale = new Sale {
+                  Amount = 1000,
+
+              };*/
+            /*Sale sale = null;
             //int?はnull許容型、[?.]はnull条件演算子0
             int? ret = sale?.Amount;
-            Console.WriteLine(ret);
+            Console.WriteLine(ret);*/
             #endregion
 
             Console.Write("整数を入力:");
             string jnpuntNum = Console.ReadLine();
-            int num;
-
-            if (int.TryParse(jnpuntNum, out num)) {
-                Console.WriteLine("整数に変換した値：" + num);
-            } else {
-                Console.WriteLine("変換に失敗しました。");
+            try {
+                int num = int.Parse(jnpuntNum);
             }
+            catch (FormatException ex) {
+                Console.WriteLine("FormatException" + ex.Message);
+
+            }
+            catch (ArgumentException ex) {
+                Console.WriteLine("rgumentException" + ex.Message);
+            }
+            catch (OverflowException ex) {
+                Console.WriteLine("OverflowException" + ex.Message);
+            }
+            finally {
+                Console.WriteLine("処理が終了しました。");
+            }
+
+
 
 
 
