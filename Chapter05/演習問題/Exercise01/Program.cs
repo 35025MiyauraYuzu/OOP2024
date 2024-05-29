a1fce00b3d6d716641714f4f190b94e63c892e12
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-05-29 15:42:05 +0900

    5章演習問題　5.2まで

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str= Console.ReadLine();
+            var ag = Console.ReadLine();
+            if (String.Compare(str, ag, true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var math =Console.ReadLine();
+            int.TryParse(math, out var n);
+            Console.WriteLine(n.ToString("#,0"));
+        }
+    }
+}
