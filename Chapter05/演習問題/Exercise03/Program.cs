dad829cddfa9d6f16f87df5f9406a14dcf932e76
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-05-30 15:46:38 +0900

    演習問題　5.3.5

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Exercise03 {
     internal class Program {
@@ -44,26 +45,37 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_3(string text) {
-            
+
             // foreach (var item in text) {
             //      if (item == ' ') {
             //        count++;
             //     }
             //  }
             //  Console.WriteLine(count);
-            int count =text.Split(' ').Length;
+            int count = text.Split(' ').Length;
             Console.WriteLine("単語数:{0}", count);
-
         }
 
-
         private static void Exercise3_4(string text) {
+            var words = text.Split(' ').Where(s => s.Length <= 4);
 
-
+            foreach (var word in words) {
+                Console.WriteLine(word);
+            }
         }
 
         private static void Exercise3_5(string text) {
 
+            var array = text.Split(' ').ToArray();
+
+            var sb = new StringBuilder();
+            foreach (var item in array) {
+                sb.Append(item);
+                sb.Append(' ');
+
+            }
+            Console.WriteLine(sb);
         }
+
     }
 }
