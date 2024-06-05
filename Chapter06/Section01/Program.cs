bd3edcf73058f37994100547a910c936f20edd39
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-06-05 13:11:53 +0900

    第6章演習問題　

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //var numbers =Enumerable.Repeat(-1,20).ToList();
+
+            var pages = Books.GetBooks().Max(x => x.Pages);
+            var booktitle = Books.GetBooks().OrderByDescending(x => x.Price).ToList();
+
+            foreach (var book in booktitle) {
+                Console.WriteLine(book.Title + ":" + book.Price);
+            }
+
+          //  booktitle.ForEach(b => Console.WriteLine(b.Title + ":" + b.Price));
+        }
+    }
+}
