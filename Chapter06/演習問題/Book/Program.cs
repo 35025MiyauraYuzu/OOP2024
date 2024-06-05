d78ea4d271b2380076dc32da5cfddb4d7f9197a7
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-06-05 14:25:16 +0900

    第6章　演習問題2.1

---
+++ Chapter06/演習問題/Book/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book {
+    internal class Program {
+        static void Main(string[] args) {
+            class Book {
+            public string Title { get; set; }
+            public int Price { get; set; }
+            public int Pages { get; set; }
+        }
+    }
+    }
+}
