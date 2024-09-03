ddc68acaa58c6fa209e092f79099499380912546
結寿 宮浦 <oibc35025@ojs.ac.jp>
2024-09-03 12:07:16 +0900

    途中

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -21,8 +21,14 @@ namespace RssReader {
                 var url = wc.OpenRead(tbRssUrl.Text);
                 var xdoc = XDocument.Load(url);
                 var xtitles = xdoc.Root.Descendants("item").Elements("title").Select(x => x.Value);
+                //Select(x => new {
+                //  Title = x.Elements("title").Select(y => y.Value),
+                //  Link = x.Elements("link").Select(y => y.Value),
+                // });
 
 
+                //Elements("title").Select(x => x.Value);
+                //.Select(Item=>item.Elements("title").Value);
                 foreach (var xtitle in xtitles) {
                     lbRssTitle.Items.Add(xtitle);
                 }
