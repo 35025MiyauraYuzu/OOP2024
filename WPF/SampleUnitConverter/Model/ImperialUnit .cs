c919142a5c35bcbfcc8ebb6469e799a15f2582ab
MiyauraYuzu <166583838+35264IwaiSora@users.noreply.github.com>
2024-11-06 14:22:03 +0900

    SampleUnitConverter完成！！！！！！！！！！！！！！！！！！！！！！

---
+++ WPF/SampleUnitConverter/Model/ImperialUnit .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    //ヤード単位を表すクラス
+    public class ImperialUnit :DistanceUinit{
+        private static List<ImperialUnit> units = new List<ImperialUnit> {
+            new ImperialUnit {Name ="in", Conefficient = 1,},
+            new ImperialUnit {Name ="ft", Conefficient =12,},
+            new ImperialUnit {Name ="yd", Conefficient =12* 3,},
+            new ImperialUnit {Name ="ml", Conefficient =12*3*1760},
+        };
+
+        public static ICollection<ImperialUnit> Units { get { return units; } }
+
+        /// <summary>
+        ///     メートル単位からヤード単位に変換
+        /// </summary>
+        /// <param name="unit">メートル単位</param>
+        /// <param name="value">値</param>
+        /// <returns></returns>
+        public double FromMetricunit(MetricUnit unit,double value) {
+            return(value * unit.Conefficient)/25.4/this.Conefficient;
+
+        }
+
+    }
+}
---
+++ WPF/SampleUnitConverter/Model/MetricUnit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter {
+    //メートル単位を表すクラス
+    public class MetricUnit :DistanceUinit{
+        private static List<MetricUnit> units = new List<MetricUnit> {
+            new MetricUnit {Name ="mm", Conefficient = 1,},
+            new MetricUnit {Name ="cm", Conefficient =10,},
+            new MetricUnit {Name ="m", Conefficient =10* 100,},
+            new MetricUnit {Name ="km", Conefficient =10*100*1000},
+        };
+
+        public static ICollection<MetricUnit> Units { get { return units; } }
+
+        /// <summary>
+        ///     ヤード単位からメートル単位に変換
+        /// </summary>
+        /// <param name="unit">ヤード単位</param>
+        /// <param name="value">値</param>
+        /// <returns></returns>
+        public double FromImperialUnit(ImperialUnit unit,double value) {
+            return(value * unit.Conefficient)*25.4/this.Conefficient;
+
+        }
+
+    }
+}
